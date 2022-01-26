3e724fc3ff4b4e2b861318c4c94f5eba8c47b18c
Davi <davi.freitas@useall.com.br>
2022-01-26 18:15:56 -0300

    stripe

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dontenv = require("dotenv");
+const cors = require("cors");
+
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productsRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoutes = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dontenv.config();
 
@@ -17,12 +20,14 @@ mongoose.connect(process.env.MONGO_URL)
 });
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", orderRoutes);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Backend server running at ${process.env.PORT}`)
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_CHAVE;
+const stripe = require("stripe")('sk_test_51Jf0qyGkzJpr13DBvM4gu1pgZXzvv7iPah6h16XVPSi5owuWrippvZsDjX19LoQUcTLkGOvZHUxefg0ZRiUVhkvz00cA812qhi');
 
 router.post("/payment", (request, response) => {
     stripe.charges.create({
